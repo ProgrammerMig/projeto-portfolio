42bff4e88ec7d7ef2bfc68039ad03d4e320590c3
ProgrammerMig <migueltcavalheiro@gmail.com>
2024-01-03 15:12:01 -0300

    Organização e Criação dos arquivos.
    03 de Janeiro de 2024
    
    Criei toda a estrutura do site.
    Adicionei a Home, seu estilo e seu script.
    Adicionei todos os "Assets" da Home.

---
+++ Páginas/Home/home.js
@@ -0,0 +1,65 @@
+dataAtual = new Date()
+
+aimg = document.getElementById('a-img') 
+
+mig = document.querySelector('img.pointer_mig')
+
+ano = dataAtual.getFullYear()
+anoatual = parseInt(ano)
+pgc = document.querySelector('b.pgc')
+ts_img = document.querySelector('img.ts-img')
+pg = 0
+text = document.querySelector('div.content')
+
+texts = [`Tenho atualmente ${anoatual - 2010} anos! 👦`, `Curto temas como: espaço🌌, gaming🎮, animação✍, entre outras coisas...`, `Sei as linguagens: Python🐍, Javascript e um pouco de PHP🐘`, `Gosto de muitos animais, mas de todos eles a Raposa é uma de minhas preferidas! 🦊`, `Tenho como cor favorita, Tons azulados 🔵 e roxos. 🟣`]
+
+function nextpage(){ 
+    pg++
+
+    if(pg > 5){
+        pgc.innerHTML = `[1 | 5]`
+        pg = 1
+        text.innerHTML = texts[0]
+    } else {
+        pgc.innerHTML = `[${pg} | 5]`
+        if(pg == 1){
+            text.innerHTML = texts[0]
+        } else if(pg == 2){
+            text.innerHTML = texts[1]
+        } else if(pg == 3){
+            text.innerHTML = texts[2]
+        } else if(pg == 4){
+            text.innerHTML = texts[3]
+        } else if(pg == 5){
+            text.innerHTML = texts[4]
+        } else {
+            text.innerHTML = texts[1]
+        }
+        
+    }
+    ts_img.src = `Home_Assets/mig_${pg}.png`
+}
+
+
+function mostrar(){
+    aimg.classList.remove('invisible')
+    aimg.classList.add('shown')
+}
+
+function esconder(){
+    aimg.classList.remove('shown')
+    aimg.classList.add('invisible')
+    
+}
+
+function mig_idle(){
+    mig.src = 'Home_Assets/mig_idle.png'
+}
+
+function mig_left(){
+    mig.src = 'Home_Assets/mig_left.png'
+}
+
+function mig_right(){
+    mig.src = 'Home_Assets/mig_right.png'
+}
